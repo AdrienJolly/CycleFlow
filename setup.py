d8520075707a9b447b8039dec99a94bde56e1f23
Adrien Jolly <adrienjolly@AdminisatorsMBP.fritz.box>
2021-01-13 23:37:07 +0100

    update of folder structure

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="CycleFlow",
-    version="1.0",
+    version="1.1",
     author="Adrien Jolly and Nils B. Becker",
     author_email="a.jolly@dkfz.de",
     description="Quantification of Cell Cycle length and cycling heterogeneity",
     url="https://github.com/AdrienJolly/CycleFlow",
-    install_requires=['numpy', 'numba','scipy','pandas']
+    install_requires=['numpy', 'numba','scipy','pandas'],
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                ]
 )
